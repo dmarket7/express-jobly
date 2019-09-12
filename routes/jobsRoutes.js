f698d50806219abe84f93e34c902921c601d760f
Dario Mercado <dariomercado@RICHARDs-MBP.localdomain>
2019-09-12 09:45:43 -0700

    initial job routes

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const ExpressError = require('../helpers/expressError');
+
+const Job = require('../models/jobs');
+
+
+
+
+module.exports = router;
