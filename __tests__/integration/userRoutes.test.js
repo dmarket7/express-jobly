71543689ea356a40b753d40ddc69709e445863dd
Dario Mercado <dariomercado@RICHARDs-MBP.localdomain>
2019-09-13 11:10:56 -0700

    fixed user routes tests

--- __tests__/integration/userRoutes.test.js
+++ __tests__/integration/userRoutes.test.js
@@ -5,20 +5,23 @@ const User = require('../../models/users')
 process.env.NODE_ENV = "test";
 
 describe("Users Routes Tests", function () {
-
+  let user;
   beforeEach(async function () {
     await db.query("DELETE FROM users");
 
-    await User.create({
-      username: "user1",
-      password: "abc123",
-      first_name: "John",
-      last_name: "Doe",
-      email: "user1@email.com"
-    });
+    user = await request(app)
+      .post('/users')
+      .send({
+        username: "user15",
+        password: "abc123",
+        first_name: "John",
+        last_name: "Doe",
+        email: "user1@email.com",
+        is_admin: false
+      });
 
     await User.create({
-      username: "user2",
+      username: "user25",
       password: "abc123",
       first_name: "Jane",
       last_name: "Doe",
@@ -32,12 +35,12 @@ describe("Users Routes Tests", function () {
       .get('/users')
 
     expect(users.body.users).toEqual([{
-        username: "user1",
+        username: "user15",
         first_name: "John",
         last_name: "Doe",
         email: "user1@email.com"
       }, {
-        username: "user2",
+        username: "user25",
         first_name: "Jane",
         last_name: "Doe",
         email: "user2@email.com"
@@ -46,9 +49,9 @@ describe("Users Routes Tests", function () {
 
   test('Gets one user from /users/:username', async function () {
     const response = await request(app)
-      .get('/users/user1')
+      .get('/users/user15')
 
-      expect(response.body.user.username).toBe("user1");
+      expect(response.body.user.username).toBe("user15");
       expect(response.body.user.first_name).toBe("John");
       expect(response.body.user.last_name).toBe("Doe");
       expect(response.body.user.email).toBe("user1@email.com");
@@ -59,14 +62,14 @@ describe("Users Routes Tests", function () {
     const response = await request(app)
       .post('/users')
       .send({
-        username: "user3",
+        username: "user35",
         password: "abc123",
         first_name: "Jack",
         last_name: "Doe",
         email: "user3@email.com"
       })
 
-    expect(response.body.user.username).toBe("user3");
+    expect(response.body.user.username).toBe("user35");
     expect(response.body.user.first_name).toBe("Jack");
     expect(response.body.user.last_name).toBe("Doe");
     expect(response.body.user.email).toBe("user3@email.com");
@@ -74,15 +77,16 @@ describe("Users Routes Tests", function () {
 
   test('Can PATCH a user at /users/:username', async function () {
     const response = await request(app)
-      .patch('/users/user2')
+      .patch('/users/user25')
       .send({
         first_name: "Jack",
         last_name: "Doe",
         email: "user3@email.com",
         photo_url: "https://pbs.twimg.com/profile_images/506219037475344384/6oOxfIXB_400x400.png",
+        _token: user.body.token
       })
 
-    expect(response.body.user.username).toBe("user2");
+    expect(response.body.user.username).toBe("user25");
     expect(response.body.user.first_name).toBe("Jack");
     expect(response.body.user.last_name).toBe("Doe");
     expect(response.body.user.email).toBe("user3@email.com");
@@ -91,7 +95,10 @@ describe("Users Routes Tests", function () {
 
   test('Can DELETE a user at /users/:username', async function () {
     const response = await request(app)
-      .delete('/users/user1')
+      .delete('/users/user15')
+      .send({
+        _token: user.body.token
+      })
     
       expect(response.body.message).toBe("User deleted.");
   });
